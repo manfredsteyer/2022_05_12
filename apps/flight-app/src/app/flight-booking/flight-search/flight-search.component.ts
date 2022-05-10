c139f96b2596da1156bd5afd41dd54e8773574d1
Manfred Steyer <manfred.steyer@gmx.net>
2022-05-10 13:17:39 +0200

    tag 1, vormittag

--- apps/flight-app/src/app/flight-booking/flight-search/flight-search.component.ts
+++ apps/flight-app/src/app/flight-booking/flight-search/flight-search.component.ts
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
 /* eslint-disable @angular-eslint/no-empty-lifecycle-method */
 import {Component, OnInit} from '@angular/core';
+
 import {FlightService} from '@flight-workspace/flight-lib';
 
 @Component({
